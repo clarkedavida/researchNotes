dffc80c8e959e974e08bab4133c948be3aebae7f
david clarke <clarke.davida@gmail.com>
2024-04-21 16:13:26 -0600

    more matrix calc stuff

--- spotContractions.bash
+++ spotContractions.bash
@@ -12,9 +12,13 @@ for file in *.tex
 do
     sed -i 's/e'\''ll/e will/g' $file
     sed -i 's/sn'\''t/s not/g' $file
+    sed -i 's/can'\''t/cannot/g' $file
+    sed -i 's/don'\''t/do not/g' $file
     sed -i 's/dn'\''t/d not/g' $file
 done
 
+grep "don't" *.tex
+grep "can't" *.tex
 grep "e'll" *.tex
 grep "sn't" *.tex
 grep "dn't" *.tex
